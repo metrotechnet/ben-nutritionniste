3da8f8b1df283475a0a44ee61a60505f6422f582
metrotechnet <denis_boulanger@hotmail.com>
2025-12-20 18:21:47 -0500

    Update favicon format and integrate Firebase Analytics via CDN

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,11 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase Analytics via CDN (no ES module imports)
+// Make sure to include these in your HTML before this script:
+// <script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js"></script>
+// <script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics-compat.js"></script>
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCySPtmLDkCZtPA46DxdFyJN2rlMoTcYTs",
   authDomain: "bennutritionniste.firebaseapp.com",
@@ -16,6 +13,5 @@ const firebaseConfig = {
   measurementId: "G-LB3NMLVC31"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
