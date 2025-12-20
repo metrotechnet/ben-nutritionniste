dc8bfdfdafc20a8bd607d0b5981c183171bc407a
metrotechnet <denis_boulanger@hotmail.com>
2025-12-20 14:09:13 -0500

    Refactor code structure for improved readability and maintainability

--- deploy.bat
+++ deploy.bat
@@ -3,10 +3,10 @@
 # Simple and reliable Firebase deployment
 # =======================================================
 
-Remove-Item -Recurse -Force .\public\*
-Copy-Item -Recurse -Force .\index.html .\public\
-Copy-Item -Recurse -Force .\css .\public\
-Copy-Item -Recurse -Force .\js .\public\
-Copy-Item -Recurse -Force .\assets .\public\
+@REM Remove-Item -Recurse -Force .\public\*
+@REM Copy-Item -Recurse -Force .\index.html .\public\
+@REM Copy-Item -Recurse -Force .\css .\public\
+@REM Copy-Item -Recurse -Force .\js .\public\
+@REM Copy-Item -Recurse -Force .\assets .\public\
 firebase deploy --only hosting --force
     
